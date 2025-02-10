d00fbcfb9c14b55e4bc1153b2a5f9f3571485dd5
Timur <bory1914@gmail.com>
2025-02-11 04:40:54 +0500

    homeWrok03

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homeworks03/OnboardingScreen.kt
@@ -0,0 +1,39 @@
+package org.wikipedia.homeworks.homeworks03
+
+val skipButton = listOf(
+    "Class" to "FrameLayout",
+    "Id" to "fragment_onboarding_skip_button",
+    "text" to "Skip"
+)
+
+val drawable = listOf(
+    "Class" to "FrameLayout",
+    "Id" to "view_onboarding_page_indicator",
+)
+
+
+val contineButton = listOf(
+    "Class" to "FrameLayout",
+    "Id" to "fragment_onboarding_forward_button",
+    "Text" to "Continue"
+)
+
+
+val imageViewCentered = listOf(
+    "Class" to "OnboardingPageView ",
+    "Id" to "imageViewCentered"
+)
+
+val languageListContainer = listOf(
+    "Class" to "LinarLaout"
+)
+
+val secondaryTextView = listOf(
+    "Class" to "LinarLaout",
+    "Id" to "secondaryTextView"
+)
+val buttonLanguage = listOf(
+    "Class" to "addLanguageButton",
+    "Id" to "languageListContainer",
+    "Text" to "Add or edit languages"
+)
